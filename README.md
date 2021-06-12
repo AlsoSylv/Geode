7c341324fef22723b75db777d0446d4e391f8d22
AlsoNiko <64978191+AlsoNiko@users.noreply.github.com>
2021-06-12 16:44:04 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -2,10 +2,6 @@ Geode
 
 Gedoe allows items to drop from stone when mined
 
-Future features planned are 
-Nuggests from netherrack
-EXP from stone
-Treasure chests from stone 
+Future features planned are Nuggests from netherrack EXP from stone Treasure chests from stone
 
-Complining:
-Please use gradlew build when compiling
+Complining: Please use gradlew build when compiling
--- src/main/java/com/AlsoNiko/stonedrops/stonedrops.java
+++ src/main/java/com/AlsoNiko/stonedrops/stonedrops.java
@@ -1,8 +1,11 @@
 package com.AlsoNiko.stonedrops;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
 import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.loot.ConstantLootTableRange;
 import net.minecraft.loot.entry.LootTableEntry;
 import net.minecraft.util.Identifier;
@@ -63,5 +66,20 @@ public class stonedrops implements ModInitializer {
                 supplier.withPool((poolBuilder5.build()));
             }
         }));
+
+        PlayerBlockBreakEvents.AFTER.register((world, player, pos, state, blockEntity) -> {
+            if (state.getBlock() == Blocks.STONE) {
+                world.spawnEntity(new ExperienceOrbEntity(world, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 1));
+            }
+            if (state.getBlock() == Blocks.DIORITE) {
+                world.spawnEntity(new ExperienceOrbEntity(world, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 1));
+            }
+            if (state.getBlock() == Blocks.ANDESITE) {
+                world.spawnEntity(new ExperienceOrbEntity(world, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 1));
+            }
+            if (state.getBlock() == Blocks.GRANITE) {
+                world.spawnEntity(new ExperienceOrbEntity(world, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 1));
+            }
+        });
     }
 }
