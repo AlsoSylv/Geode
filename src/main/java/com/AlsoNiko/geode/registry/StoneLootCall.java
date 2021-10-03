770be847acab3dd914c0da97ad85a96e7c517a90
AlsoNiko <josephao424@gmail.com>
2021-10-02 21:06:28 -0400

    Fix massive bug, remove debug loot table

--- src/main/java/com/AlsoNiko/geode/registry/StoneLootCall.java
+++ src/main/java/com/AlsoNiko/geode/registry/StoneLootCall.java
@@ -8,24 +8,60 @@ import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 public class StoneLootCall {
     public static void modifyLootTables() {
             LootTableLoadingCallback.EVENT.register((resourceManager, lootmanager, id, supplier, setter) -> {
+            if (LootTableRegistry.Die_Loot_Table_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder2 = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .with(LootTableEntry.builder(LootTableRegistry.Rum_loot_table_ID));
+                supplier.withPool(poolBuilder2.build());
+            }
 
-                LootTableRegistry.Stone_Loot_Table_ID.equals (id);
-                LootTableRegistry.And_Loot_Table_ID.equals (id);
-                LootTableRegistry.Die_Loot_Table_ID.equals (id);
-                LootTableRegistry.Gran_Loot_Table_ID.equals (id);
-                LootTableRegistry.Deep_Loot_Table_ID.equals (id);
-                LootTableRegistry.Drip_Loot_Table_ID.equals (id);
-                LootTableRegistry.Tuff_Loot_Table_ID.equals (id);
-                FabricLootPoolBuilder poolBuilder4 = FabricLootPoolBuilder.builder ()
-                        .rolls (ConstantLootNumberProvider.create (1))
-                        .with (LootTableEntry.builder (LootTableRegistry.Rum_loot_table_ID));
-                supplier.withPool (poolBuilder4.build ());
+            if (LootTableRegistry.And_Loot_Table_ID.equals(id)) {
+                    FabricLootPoolBuilder poolBuilder2 = FabricLootPoolBuilder.builder()
+                            .rolls(ConstantLootNumberProvider.create(1))
+                            .with(LootTableEntry.builder(LootTableRegistry.Rum_loot_table_ID));
+                    supplier.withPool(poolBuilder2.build());
+            }
+            
+            if (LootTableRegistry.Gran_Loot_Table_ID.equals(id)) {
+                    FabricLootPoolBuilder poolBuilder2 = FabricLootPoolBuilder.builder()
+                            .rolls(ConstantLootNumberProvider.create(1))
+                            .with(LootTableEntry.builder(LootTableRegistry.Rum_loot_table_ID));
+                    supplier.withPool(poolBuilder2.build());
+            }
 
-                LootTableRegistry.Rack_Loot_Table_ID.equals (id);
-                FabricLootPoolBuilder poolBuilder5 = FabricLootPoolBuilder.builder ()
-                        .rolls (ConstantLootNumberProvider.create (1))
-                        .with (LootTableEntry.builder (LootTableRegistry.Gin_Loot_Table_ID));
-                supplier.withPool (poolBuilder5.build ());
-            });
+            if (LootTableRegistry.Stone_Loot_Table_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .with(LootTableEntry.builder(LootTableRegistry.Rum_loot_table_ID));
+                supplier.withPool(poolBuilder.build());
+
+            }
+
+            if (LootTableRegistry.Rack_Loot_Table_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder5 = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .with(LootTableEntry.builder(LootTableRegistry.Gin_Loot_Table_ID));
+                supplier.withPool((poolBuilder5.build()));
+            }
+            if (LootTableRegistry.Deep_Loot_Table_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder6 = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .with(LootTableEntry.builder(LootTableRegistry.Rum_loot_table_ID));
+                supplier.withPool((poolBuilder6.build()));
+            }
+            if (LootTableRegistry.Tuff_Loot_Table_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder7 = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .with(LootTableEntry.builder(LootTableRegistry.Rum_loot_table_ID));
+                supplier.withPool((poolBuilder7.build()));
+            }
+            if (LootTableRegistry.Drip_Loot_Table_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder8 = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .with(LootTableEntry.builder(LootTableRegistry.Rum_loot_table_ID));
+                supplier.withPool((poolBuilder8.build()));
+            }
+        });
+
     }
 }
